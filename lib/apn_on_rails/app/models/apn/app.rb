4bf324789e546a34750dee2c7a69bfc1c7ab0494
Logan Sease <lsease@gmail.com>
2016-03-23 09:39:12 -0400

    rails 4 fixes

--- lib/apn_on_rails/app/models/apn/app.rb
+++ lib/apn_on_rails/app/models/apn/app.rb
@@ -59,7 +59,7 @@ class APN::App < APN::Base
 
       begin
         APN::Connection.open_for_delivery({:cert => the_cert, :host => host}) do |conn, sock|
-          APN::Device.where(conditions) do |dev|
+          APN::Device.where(conditions).each do |dev|
             dev.unsent_notifications.each do |noty|
               conn.write(noty.message_for_sending)
               noty.sent_at = Time.now
