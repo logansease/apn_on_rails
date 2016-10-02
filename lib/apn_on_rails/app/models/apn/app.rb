13fef288a38247cbeb49d410f85468ddfcd420a6
Logan Sease <lsease@gmail.com>
2016-10-02 12:46:41 -0400

    update for sending performance

--- lib/apn_on_rails/app/models/apn/app.rb
+++ lib/apn_on_rails/app/models/apn/app.rb
@@ -59,10 +59,9 @@ class APN::App < APN::Base
       begin
         APN::Connection.open_for_delivery({:cert => the_cert, :host => host}) do |conn, sock|
             notifications = APN::Notification.joins(:device).where(:apn_devices => {:app_id => app_id}).where(:sent_at => nil)
+            notifications.update_all :sent_at => Time.now
             notifications.each do |noty|
               conn.write(noty.message_for_sending)
-              noty.sent_at = Time.now
-              noty.save
             end
         end
 
